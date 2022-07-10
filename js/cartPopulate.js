92d2cd61b94bbbd411d475e341fc09d4bd307559
hlogaraj <hlogaraj@gmail.com>
2022-07-09 20:41:31 -0500

    x

--- js/cartPopulate.js
+++ js/cartPopulate.js
@@ -35,19 +35,3 @@ if (cartItems != null) {
 }
 
 
-function addToCart(listing) {
-    let name = listing.id;
-    let productInfo = products[name]
-    let cartInfo = []
-    let quantity;
-    if (cartItems[name] != null) {
-        quantity = 1;
-    } else {
-        quantity++; 
-    }
-    cartInfo = [quantity, productInfo];
-    cartItems[name] = cartInfo;
-    localStorage.setItem("cart items", cartItems);
-    console.log(cartItems);
-}
-
