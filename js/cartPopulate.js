f9ba40bb1cf9ed417024901591c242b96e0b8a8e
hlogaraj <hlogaraj@gmail.com>
2022-07-09 21:29:49 -0500

    x

--- js/cartPopulate.js
+++ js/cartPopulate.js
@@ -8,14 +8,14 @@ if (products != null) {
 
 if (cartItems != null) {
     console.log("cart items found");
+    let table = document.getElementById("cart-items");
+    let totalPrice = 0;
     for (let i = 0; i < cartItems.length; i++) {
         let item = cartItems[i];
         let name = String(item.name);
         let quantity = parseInt(item.quantity);
         let price = parseFloat(item.price);
         let qtyPrice = quantity * price;
-
-        let table = document.getElementById("cart-items");
 
         let row = document.createElement("tr");
         row.id = name;
@@ -24,14 +24,25 @@ if (cartItems != null) {
         let quantityData = document.createElement("td");
         quantityData.innerHTML = quantity;
         let priceData = document.createElement("td");
-        priceData.innerHTML = qtyPrice;
+        priceData.innerHTML = "$" + qtyPrice;
 
         row.appendChild(nameData);
         row.appendChild(quantityData);
         row.appendChild(priceData);
 
         table.appendChild(row);
+
+        totalPrice += qtyPrice;
     }
+    let row = document.createElement("tr");
+    row.id = "total";
+    let totalLabel = document.createElement("h3");
+    totalLabel.innerHTML = "Total Price:";
+    let totalLabelBox = document.createElement("td");
+    totalLabelBox.appendChild(totalLabel);
+    let totalData = document.createElement("td");
+    totalData.colSpan = "2";
+    totalData.innerHTML = "$" + totalPrice;
 }
 
 
